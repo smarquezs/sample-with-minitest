62e00ff83f78f8853710701d1e01d001014a951f
Sergio Márquez <smarquezs@gmail.com>
2014-09-28 17:27:31 -0300

    First commit; ADD: rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SampleWithNimitest::Application.config.secret_key_base = '6077de3aa79789eb8ddad1ca7030a8d5f9424ce6a438558de01e47ef91b702a2c0ffa466c705b4dbcddd70a3d4a9a91b0318565ee1c8c94833d6eba89df4e587'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+SampleWithNimitest::Application.config.session_store :cookie_store, key: '_sample-with-nimitest_session'
